376c94c4e313a51dabbb5ef72fb41fcc868a4e59
Giovanni Ruggirello <grsruggirello25@gmail.com>
2014-11-16 19:34:04 -0500

    User.java update
    Added variables to user.java and gave void function variables to pull
    from.

--- lib/src/main/java/com/xlythe/textmanager/User.java
+++ lib/src/main/java/com/xlythe/textmanager/User.java
@@ -4,8 +4,12 @@ package com.xlythe.textmanager;
  * Represents a person who can send or receive messages.
  */
 public interface User {
-    /**
-     * Return the user's name
-     * */
-    public void getName();
-}
+        String sender = "";
+        int PhoneNumber = 0;
+
+        /**
+         * Return the user's name
+         * */
+        public void getName(String sender, int PhoneNumber);
+    }
+
