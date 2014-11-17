37ecda87e3305b3e41dbe943320dc1a8adc5bce8
Giovanni Ruggirello <grsruggirello25@gmail.com>
2014-11-16 20:09:15 -0500

    User.java
    No update

--- lib/src/main/java/com/xlythe/textmanager/User.java
+++ lib/src/main/java/com/xlythe/textmanager/User.java
@@ -4,20 +4,11 @@ package com.xlythe.textmanager;
  * Represents a person who can send or receive messages.
  */
 public interface User {
-<<<<<<< HEAD
         String sender = "";
         int PhoneNumber = 0;
 
         /**
          * Return the user's name
          * */
-        public void getName(String sender, int PhoneNumber);
+        public String getName(String sender, int PhoneNumber);
     }
-
-=======
-    /**
-     * Return the user's name
-     * */
-    public String getName();
-}
->>>>>>> origin/master
