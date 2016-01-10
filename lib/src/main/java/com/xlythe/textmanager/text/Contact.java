30c7c2a4847560a0c46c9b9408f05ee9c53edc07
Will <xlythe@gmail.com>
2016-01-10 15:06:04 -0800

    add a method to grab all contacts + query for phone numbers/emails

--- lib/src/main/java/com/xlythe/textmanager/text/Contact.java
+++ lib/src/main/java/com/xlythe/textmanager/text/Contact.java
@@ -9,34 +9,37 @@ import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Email;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 
 import com.xlythe.textmanager.User;
 import com.xlythe.textmanager.text.util.Utils;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Represents a phone number.
  */
 public final class Contact implements User, Parcelable {
-
+    private final long mId;
     private final String mNumber;
     private final String mDisplayName;
     private final String mPhotoUri;
     private final String mPhotoThumbUri;
 
     protected Contact(Cursor c) {
+        mId = c.getLong(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
         // Number may not exist, so check first
         int column = c.getColumnIndex(ContactsContract.PhoneLookup.NUMBER);
-        if (column != -1) {
-            mNumber = c.getString(c.getColumnIndex(ContactsContract.PhoneLookup.NUMBER));
-        } else {
-            mNumber = "";
-        }
+        mNumber = column != -1 ? c.getString(column) : null;
         mDisplayName = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
         mPhotoUri = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.PHOTO_URI));
         mPhotoThumbUri = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI));
     }
 
     protected Contact(String address) {
+        mId = -1;
         mNumber = address;
         mDisplayName = null;
         mPhotoUri = null;
@@ -44,14 +47,91 @@ public final class Contact implements User, Parcelable {
     }
 
     private Contact(Parcel in) {
+        mId = in.readLong();
         mNumber = in.readString();
         mPhotoUri = in.readString();
         mDisplayName = in.readString();
         mPhotoThumbUri = in.readString();
     }
 
+    public String getId() {
+        return Long.toString(mId);
+    }
+
+    public long getIdAsLong() {
+        return mId;
+    }
+
     public String getNumber() {
         return mNumber;
+    }
+
+    public String getNumber(Context context) {
+        return getNumber();
+    }
+
+    public List<String> getEmails(Context context) {
+        List<String> emailAddresses = new LinkedList<>();
+
+        Uri uri = Email.CONTENT_URI;
+        String[] projection = new String[] {
+                Email.DATA,
+                Email.IS_PRIMARY
+        };
+        String clause = Email.CONTACT_ID + " = ?";
+        String[] args = new String[] { getId() };
+        Cursor cursor = context.getContentResolver().query(uri, projection, clause, args, null);
+        if (cursor != null) {
+            try {
+                if (cursor.moveToFirst()) {
+                    do {
+                        boolean isPrimary = cursor.getInt(cursor.getColumnIndex(Email.IS_PRIMARY)) != 0;
+                        String address = cursor.getString(cursor.getColumnIndex(Email.DATA));
+
+                        if (isPrimary) {
+                            emailAddresses.add(0, address);
+                        } else {
+                            emailAddresses.add(address);
+                        }
+                    } while (cursor.moveToNext());
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        return emailAddresses;
+    }
+
+    public List<String> getNumbers(Context context) {
+        List<String> phoneNumbers = new LinkedList<>();
+
+        Uri uri = Phone.CONTENT_URI;
+        String[] projection = new String[] {
+                Phone.NUMBER,
+                Phone.IS_PRIMARY
+        };
+        String clause = Phone.CONTACT_ID + " = ?";
+        String[] args = new String[] { getId() };
+        Cursor cursor = context.getContentResolver().query(uri, projection, clause, args, null);
+        if (cursor != null) {
+            try {
+                if (cursor.moveToFirst()) {
+                    do {
+                        boolean isPrimary = cursor.getInt(cursor.getColumnIndex(Phone.IS_PRIMARY)) != 0;
+                        String number = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
+
+                        if (isPrimary) {
+                            phoneNumbers.add(0, number);
+                        } else {
+                            phoneNumbers.add(number);
+                        }
+                    } while (cursor.moveToNext());
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        return phoneNumbers;
     }
 
     public String getDisplayName() {
@@ -67,7 +147,7 @@ public final class Contact implements User, Parcelable {
     }
 
     public Uri getPhotoThumbUri(){
-        return mPhotoThumbUri!=null ? Uri.parse(mPhotoThumbUri) : null;
+        return mPhotoThumbUri != null ? Uri.parse(mPhotoThumbUri) : null;
     }
 
     public Uri getPhotoUri(){
@@ -115,6 +195,7 @@ public final class Contact implements User, Parcelable {
 
     @Override
     public void writeToParcel(Parcel out, int flags) {
+        out.writeLong(mId);
         out.writeString(mNumber);
         out.writeString(mPhotoUri);
         out.writeString(mDisplayName);
@@ -138,6 +219,21 @@ public final class Contact implements User, Parcelable {
 
         public Contact getContact() {
             return new Contact(this);
+        }
+    }
+
+    public enum Sort {
+        Alphabetical(ContactsContract.Contacts.DISPLAY_NAME + " ASC"),
+        FrequentlyContacted(ContactsContract.Contacts.TIMES_CONTACTED + " ASC");
+
+        private final String key;
+
+        Sort(String key) {
+            this.key = key;
+        }
+
+        public String getKey() {
+            return key;
         }
     }
 }
--- lib/src/main/java/com/xlythe/textmanager/text/TextManager.java
+++ lib/src/main/java/com/xlythe/textmanager/text/TextManager.java
@@ -358,11 +358,24 @@ public class TextManager implements MessageManager<Text, Thread, Contact> {
         }
     }
 
-    public Contact.ContactCursor getContactCursor(String partialName) {
+    public Contact.ContactCursor getContactCursor() {
+        return getContactCursor(Contact.Sort.Alphabetical);
+    }
+
+    public Contact.ContactCursor getContactCursor(Contact.Sort sortOrder) {
         ContentResolver contentResolver = mContext.getContentResolver();
-        final Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, partialName);
-        final String order = ContactsContract.Contacts.DISPLAY_NAME + " ASC";
-        return new Contact.ContactCursor(contentResolver.query(uri, null, null, null, order));
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        return new Contact.ContactCursor(contentResolver.query(uri, null, null, null, sortOrder.getKey()));
+    }
+
+    public Contact.ContactCursor getContactCursor(String partialName) {
+        return getContactCursor(partialName, Contact.Sort.Alphabetical);
+    }
+
+    public Contact.ContactCursor getContactCursor(String partialName, Contact.Sort sortOrder) {
+        ContentResolver contentResolver = mContext.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, partialName);
+        return new Contact.ContactCursor(contentResolver.query(uri, null, null, null, sortOrder.getKey()));
     }
 
     public Contact lookupContact(String phoneNumber) {
