4d1c53433f33a9666b812a10492b01f2a73a65eb
Will <xlythe@gmail.com>
2016-04-10 23:26:16 -0700

    remove keyboard dismiss on scroll

--- app/src/main/java/com/xlythe/sms/MessageActivity.java
+++ app/src/main/java/com/xlythe/sms/MessageActivity.java
@@ -255,19 +255,6 @@ public class MessageActivity extends AppCompatActivity
 
         mManager.registerObserver(mMessageObserver);
 
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if (newState > 0) {
-                    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    mgr.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
-                    mEditText.clearFocus();
-                    onAttachmentHidden();
-                }
-            }
-        });
-
         mGalleryAttachments = (ImageView) findViewById(R.id.gallery);
         mCameraAttachments = (ImageView) findViewById(R.id.camera);
         mStickerAttachments = (ImageView) findViewById(R.id.sticker);
