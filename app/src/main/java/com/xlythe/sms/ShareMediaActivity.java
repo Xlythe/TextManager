83800524dda10007235aa6c1c4ab7fb32c5aad87
Nick Bourdakos <bourdakos1@gmail.com>
2016-05-07 19:03:46 -0400

    feat: share media ui

--- app/src/main/java/com/xlythe/sms/ShareMediaActivity.java
+++ app/src/main/java/com/xlythe/sms/ShareMediaActivity.java
@@ -19,7 +19,7 @@ import com.xlythe.textmanager.text.TextManager;
 
 import java.util.ArrayList;
 
-public class ShareMediaActivity extends AppCompatActivity implements ContactAdapter.ClickListener {
+public class ShareMediaActivity extends AppCompatActivity {//implements ContactAdapter.ClickListener {
     private static final String TAG = ShareMediaActivity.class.getSimpleName();
 
     public static final String EXTRA_CONTACTS = "contacts";
@@ -27,80 +27,80 @@ public class ShareMediaActivity extends AppCompatActivity implements ContactAdap
 
     private TextManager mManager;
 
-    private ContactEditText mInputField;
-    private RecyclerView mRecyclerView;
-
-    private ContactAdapter mAdapter;
+//    private ContactEditText mInputField;
+//    private RecyclerView mRecyclerView;
+//
+//    private ContactAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_search);
+        setContentView(R.layout.activity_share_media);
         setResult(RESULT_OK);
 
         mManager = TextManager.getInstance(getBaseContext());
 
-        mInputField = (ContactEditText) findViewById(R.id.field);
-        mInputField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    setResult();
-                    finish();
-                    return true;
-                }
-                return false;
-            }
-        });
-        mInputField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                mAdapter.swapCursor(mManager.getContactCursor(mInputField.getPendingText()));
-            }
-        });
-
-        mRecyclerView = (RecyclerView) findViewById(R.id.list);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new ContactAdapter(this, mManager.getContactCursor(""));
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-
-        if (getIntent().hasExtra(EXTRA_CONTACTS)) {
-            ArrayList<Contact> contacts = getIntent().getParcelableArrayListExtra(EXTRA_CONTACTS);
-            mInputField.setContacts(contacts);
-        }
-
-        if (getIntent().hasExtra(EXTRA_CURSOR)) {
-            mInputField.setSelection(getIntent().getIntExtra(EXTRA_CURSOR, mInputField.getText().length()));
-        }
+//        mInputField = (ContactEditText) findViewById(R.id.field);
+//        mInputField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                if (actionId == EditorInfo.IME_ACTION_DONE) {
+//                    setResult();
+//                    finish();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//        mInputField.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                mAdapter.swapCursor(mManager.getContactCursor(mInputField.getPendingText()));
+//            }
+//        });
+//
+//        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+//        mRecyclerView.setHasFixedSize(true);
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        mAdapter = new ContactAdapter(this, mManager.getContactCursor(""));
+//        mRecyclerView.setAdapter(mAdapter);
+//        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+//
+//        if (getIntent().hasExtra(EXTRA_CONTACTS)) {
+//            ArrayList<Contact> contacts = getIntent().getParcelableArrayListExtra(EXTRA_CONTACTS);
+//            mInputField.setContacts(contacts);
+//        }
+//
+//        if (getIntent().hasExtra(EXTRA_CURSOR)) {
+//            mInputField.setSelection(getIntent().getIntExtra(EXTRA_CURSOR, mInputField.getText().length()));
+//        }
     }
 
-    @Override
-    public void onItemClicked(Contact contact) {
-        mInputField.insert(contact);
-    }
-
-    @Override
-    public void onBackPressed() {
-        setResult();
-        super.onBackPressed();
-    }
-
-    private void setResult() {
-        // Insert any remaining text
-        mInputField.insertPendingText();
-
-        // Send result
-        Intent intent = new Intent();
-        intent.putParcelableArrayListExtra(EXTRA_CONTACTS, mInputField.getContacts());
-        intent.putExtra(EXTRA_CURSOR, mInputField.getSelectionStart());
-        setResult(RESULT_OK, intent);
-    }
+//    @Override
+//    public void onItemClicked(Contact contact) {
+//        mInputField.insert(contact);
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        setResult();
+//        super.onBackPressed();
+//    }
+//
+//    private void setResult() {
+//        // Insert any remaining text
+//        mInputField.insertPendingText();
+//
+//        // Send result
+//        Intent intent = new Intent();
+//        intent.putParcelableArrayListExtra(EXTRA_CONTACTS, mInputField.getContacts());
+//        intent.putExtra(EXTRA_CURSOR, mInputField.getSelectionStart());
+//        setResult(RESULT_OK, intent);
+//    }
 }
