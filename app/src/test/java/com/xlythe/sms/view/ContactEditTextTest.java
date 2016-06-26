f019a129102c2b3ac70f62aad459142e21ce0f45
Nick Bourdakos <bourdakos1@gmail.com>
2016-06-26 12:58:08 -0400

    test

--- app/src/test/java/com/xlythe/sms/view/ContactEditTextTest.java
+++ app/src/test/java/com/xlythe/sms/view/ContactEditTextTest.java
@@ -31,10 +31,13 @@ import static junit.framework.Assert.assertFalse;
 @Config(sdk=23, shadows={ShadowTextManager.class, ShadowContact.class}, constants = BuildConfig.class)
 public class ContactEditTextTest {
     private static final String ME = "111-111-1111";
+    private static final Contact NAMELESS = ShadowContact.getInstance("444444444");
+
     private static final Contact ALICE = ShadowContact.getInstance("222-222-2222", "Alice");
     private static final Contact BOB = ShadowContact.getInstance("333-333-3333", "Bob Bobberson");
     private static final Contact CHARLIE = ShadowContact.getInstance("+1 234-567-8901", "Charlie");
-    private static final Contact NAMELESS = ShadowContact.getInstance("444444444");
+    private static final Contact DEBRA = ShadowContact.getInstance("+1 222-333-4444", "Me Normalized");
+
 
     private Context mContext;
     private ContactEditText mEditText;
@@ -51,10 +54,11 @@ public class ContactEditTextTest {
         // Update the owner's contact in TextManager
         ShadowTextManager shadowTextManager = (ShadowTextManager) ShadowExtractor.extract(TextManager.getInstance(mContext));
         shadowTextManager.setSelf(ME);
+        shadowTextManager.addContact(NAMELESS);
         shadowTextManager.addContact(ALICE);
         shadowTextManager.addContact(BOB);
         shadowTextManager.addContact(CHARLIE);
-        shadowTextManager.addContact(NAMELESS);
+        shadowTextManager.addContact(DEBRA);
     }
 
     @Test
@@ -97,9 +101,30 @@ public class ContactEditTextTest {
     }
 
     @Test
-    public void insertPending() {
+    public void insertPendingAlice() {
         mEditText.append(ALICE.getDisplayName());
         mEditText.insertPendingText();
         assertEquals(ALICE, mEditText.getContacts().get(0));
+    }
+
+    @Test
+    public void insertPendingBob() {
+        mEditText.append(BOB.getDisplayName());
+        mEditText.insertPendingText();
+        assertEquals(BOB, mEditText.getContacts().get(0));
+    }
+
+    @Test
+    public void insertPendingCharlie() {
+        mEditText.append(CHARLIE.getDisplayName());
+        mEditText.insertPendingText();
+        assertEquals(CHARLIE, mEditText.getContacts().get(0));
+    }
+
+    @Test
+    public void insertPendingDrake() {
+        mEditText.append(DEBRA.getDisplayName());
+        mEditText.insertPendingText();
+        assertEquals(DEBRA, mEditText.getContacts().get(0));
     }
 }
